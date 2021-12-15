b5f47a0fd841d0e2e4314267735037500cff55bb
dnpfclrnt <dnpfclrnt@gmail.com>
2021-12-15 15:40:20 +0900

    [#1] check template 3

--- task1_msa.py
+++ task1_msa.py
@@ -114,7 +114,7 @@ def main():
     print(datetime.datetime.now())
 
     template_msa = make_msa_from_file("spike_msa_template.fasta")
-    print(len(template_msa))
+    print("template msa : " + len(template_msa))
     files = ['HCOV19-ENGLAND-2021-04-19.fasta', \
              'HCOV19-ENGLAND-2021-05-03.fasta']
     # ,\
